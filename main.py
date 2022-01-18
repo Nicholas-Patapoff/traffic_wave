92d22845d15f44a064510d60f4edd4b829c8db38
Nicholas <nepatapoff@gmail.com>
2022-01-18 14:17:53 -0800

    cars now change color based on speed and the box size and step scaling are easier to edit

--- main.py
+++ main.py
@@ -5,47 +5,33 @@ import car_methods_repulsion as repulse
 import time
 import car_methods as algo
 import data_export_method as exports
-
+draw_scale = 40
+box_scale = 10
 red = (255, 0, 0)
 green = (0, 255, 0)
+white = (255, 255, 255)
 
-# leader = repulse.first_car(8, 8)
-# follower = repulse.vehicle(0, 0, repulse.sedan)
-# cars = [leader, follower]
-#
-# for step in range(100):  # using car_methods_repurlse
-#     for i in range(len(cars) - 1, 0, -1):
-#         print(step)
-#         repulse.movement_vel_update(cars, 0.02)
-#         print(str(cars[i].current_speed) + ' vel')
-#         print(str(cars[i].current_location) + ' location')
-#         print(str(cars[i-1].current_location) + ' leader car loc')
-#
-#         repulse.accel_calc(cars[i], cars[i - 1])
-#         print(str(cars[i].acceleration) + ' accel')
-
-first_vehicle = algo.first_car(6, 0, 10000)
-following_vehicle2 = algo.car(6, 0, 0, 0, 400, 900, 80, 2, 0.1, 8, 16)
-following_vehicle1 = algo.car(6, 0, 0, 0, 0, 900, 80, 2, 0.1, 8, 16)
+first_vehicle = algo.first_car(6, 30, 10000)
+following_vehicle2 = algo.car(6, 0, 0, 0, 400, 200, 80, 2, 0.1, 8, 16)
+following_vehicle1 = algo.car(6, 0, 0, 0, 0, 200, 80, 2, 0.1, 8, 16)
 vehicles = [first_vehicle, following_vehicle2, following_vehicle1]
 
 
 def main_method():
-    for step in range(6000):
+    for step in range(2000):
         for i in range(len(vehicles) - 1, 0, -1):
             print(step)
             algo.desired_dist(vehicles[i], vehicles[i - 1])
             algo.IDM_accel(vehicles[i], vehicles[i - 1])
             algo.moving(vehicles, 0.1)
-            roads_use.draw_box(vehicles[0].current_location / 100, green)
+            roads_use.draw_box(vehicles[0].current_location / draw_scale, white, box_scale)
             for x in range(len(vehicles) - 1, 0, -1):
-                roads_use.draw_box(vehicles[x].current_location / 100, red)
+                roads_use.draw_box(vehicles[x].current_location / draw_scale, roads_use.dynamic_color(vehicles[x]), box_scale)
             pygame.display.update()
-            roads_use.delete_box(vehicles[0].current_location / 100)
+            roads_use.delete_box(vehicles[0].current_location / draw_scale, box_scale)
             for x in range(len(vehicles) - 1, 0, -1):
-                roads_use.delete_box(vehicles[x].current_location / 100)
+                roads_use.delete_box(vehicles[x].current_location / draw_scale, box_scale)
         exports.export(vehicles, step)
-
 
 
 main_method()
--- roads_use.py
+++ roads_use.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 from pygame import gfxdraw
+
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
@@ -11,14 +12,24 @@ pygame.init()
 window = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption('Window')
 
-def draw_box(location, color):
-    pygame.gfxdraw.rectangle(window, (location, 300, 10, 10), color)
+
+def draw_box(location, color, box_size):
+    pygame.gfxdraw.rectangle(window, (location, 300, box_size, box_size), color)
 
 
-def delete_box(location):
-    pygame.gfxdraw.rectangle(window, (location, 300, 10, 10), black)
+def delete_box(location, box_size):
+    pygame.gfxdraw.rectangle(window, (location, 300, box_size, box_size), black)
 
 
-
-
-
+def dynamic_color(car):
+    if car.current_speed < 0:
+        r = 255
+        g = 0
+    elif car.current_speed > 255:
+        r = 0
+        g = 255
+    else:
+        r = (1 - car.current_speed / car.max_speed) * 255
+        g = 255 * car.current_speed / car.max_speed
+    color = (r, g, 0)
+    return color
